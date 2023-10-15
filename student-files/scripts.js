ce325b86f0dfda451f45bd1652051adb9ff7ceb3
faquinoortiz <144743802+faquinoortiz@users.noreply.github.com>
2023-10-14 22:09:29 -0700

    buttons

--- student-files/scripts.js
+++ student-files/scripts.js
@@ -1,11 +1,26 @@
+/**
+ * Treehouse JavaScript Techdegree 
+ * Project 5-API Public Request
+ * @Author Flor Aquino Ortiz
+ */
 
+//Will load and display the users data
+let users = [];
+
+fetchData().then(data => {
+  users = data;
+  users.forEach((user, index) => createUserCard(user, index));
+});
+
+
+//Function will fetch data from the Random User API
 async function fetchData() {
   const response = await fetch('https://randomuser.me/api/?results=12');
   const data = await response.json();
   return data.results;
 }
 
-
+//Function to create and append a user card to the gallery
 function createUserCard(user, index) {
   const gallery = document.getElementById('gallery');
   const card = document.createElement('div');
@@ -24,6 +39,7 @@ function createUserCard(user, index) {
   gallery.appendChild(card);
 }
 
+//The function will create a modal window and then display it for a user
 function openModal(user, index) {
   const modalContainer = document.createElement('div');
   modalContainer.className = 'modal-container';
@@ -39,8 +55,37 @@ function openModal(user, index) {
             <p class="modal-text">${user.cell}</p>
             <p class="modal-text">${user.location.street.number} ${user.location.street.name}, ${user.location.state}, ${user.location.postcode}</p>
             <p class="modal-text">Birthday: ${new Date(user.dob.date).toLocaleDateString()}</p>
-        </div>
+
+      </div>
     </div>
   `;
 
+//Creating a "Previous" button, the conditional statement displays only if there is a previous user to navigate to 
+  if (index > 0) {
+    const prevButton = document.createElement('button');
+    prevButton.type = 'button';
+    prevButton.id = 'modal-prev';
+    prevButton.className = 'modal-prev btn';
+    prevButton.textContent = 'Prev';
+    prevButton.addEventListener('click', () => openModal(users[index - 1], index - 1));
+    modalContainer.appendChild(prevButton);
+  }
+// Function creates a "Next" button, the 'click' event listener will set up the openModal.Allowing the user to navigate through the modal
+  if (index < users.length - 1) {
+    const nextButton = document.createElement('button');
+    nextButton.type = 'button';
+    nextButton.id = 'modal-next';
+    nextButton.className = 'modal-next btn';
+    nextButton.textContent = 'Next';
+    nextButton.addEventListener('click', () => openModal(users[index + 1], index + 1));
+    modalContainer.appendChild(nextButton);
+  }
+
+  modalContainer.querySelector('#modal-close-btn').addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+  });
+
+  document.body.appendChild(modalContainer);
 }
+
+
